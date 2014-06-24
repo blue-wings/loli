861c28847bfb4d32f89447b315cd8011f12f64c2
blue-wings <blue2048@gmail.com>
2014-06-24 19:41:05 +0800

    Revert "保管箱页面样式"
    This reverts commit 3c2416f42f2bbb81ff8a21d33924671e74830d29.

--- lolitabox/app/Modules/index/Action/safeKeepingAction.class.php
+++ lolitabox/app/Modules/index/Action/safeKeepingAction.class.php
@@ -2,25 +2,19 @@
 class safeKeepingAction extends commonAction{
 	
 	public function selfPickUp(){
-		$userid = $this->userid;
 		$userOrderSendProductDetail =M("UserOrderSendProductdetail");	
 		$total = $userOrderSendProductDetail->where("status=0")->count();
 		$userid = $this->userid;
 		import("ORG.Util.Page");
-		$p = new Page($count,12);
-		$productDetailsCount = $userOrderSendProductDetail->where("status=0 and userid=".$userid)->count();
-		if($productDetailsCount){
-			$productDetails = $userOrderSendProductDetail->where("status=0 and userid=".$userid)->order("orderid DESC")->limit($p->firstRow.','.$p->listRows)->select();
-			$product = M("Products");
-			for($i=0; $i<count($productDetails); $i++){
-				$productId = $productDetails[$i]["productid"];
-				$productRecord = $product->where("pid=".$productId)->find();
-				$productDetails[$i]["product"]= $productRecord;
-			}
-			$this->assign("productDetails",$productDetails);
-			$this->assign("page",$p->show());
+		$p = new Page($count,10);
+		$productDetails = $userOrderSendProductDetail->where("status=0")->order("orderid DESC")->limit($p->firstRow.','.$p->listRows)->select();
+		$product = M("Products");
+		for($i=0; $i<count($productDetails); $i++){
+			$productId = $productDetails[$i]["productid"];
+			$productRecord = $product->where("pid=".$productId)->find();
+			$productDetails[$i]["product"]= $productRecord;
 		}
-		$this->assign("productDetailsCount",$productDetailsCount);
+		$this->assign("productDetails",$productDetails);
 		$this->display();
 	}
 
