84ed46ebcbbefd587159d74b4163d88f0dbb19e0
blue-wings <blue2048@gmail.com>
2014-06-25 11:25:02 +0800

    safekeeping ajax page

--- lolitabox/app/Modules/index/Action/safeKeepingAction.class.php
+++ lolitabox/app/Modules/index/Action/safeKeepingAction.class.php
@@ -6,15 +6,20 @@ class safeKeepingAction extends commonAction{
 		$total = $userOrderSendProductDetail->where("status=0")->count();
 		$userid = $this->userid;
 		import("ORG.Util.Page");
-		$p = new Page($count,10);
+		$p = new Page($total,10);
 		$productDetails = $userOrderSendProductDetail->where("status=0")->order("orderid DESC")->limit($p->firstRow.','.$p->listRows)->select();
 		$product = M("Products");
+		$inventoryItem = M("InventoryItem");
 		for($i=0; $i<count($productDetails); $i++){
 			$productId = $productDetails[$i]["productid"];
 			$productRecord = $product->where("pid=".$productId)->find();
 			$productDetails[$i]["product"]= $productRecord;
+			$inventoryItemRecord = $inventoryItem->where("id=".$productRecord["inventory_item_id"])->find();
+				$productDetails[$i]["inventoryItem"]= $inventoryItemRecord;
 		}
 		$this->assign("productDetails",$productDetails);
+		$page=$p->show();	
+		$this->assign('page',$page);
 		$this->display();
 	}
 
