357a5ce0212b7549ed54a68629537312a3825839
nkxubin <nkxubin@gmail.com>
2014-07-23 18:17:41 +0800

    重构代码，修改货币的显示格式

--- lolitabox/app/Modules/index/Action/subscribeAction.class.php
+++ lolitabox/app/Modules/index/Action/subscribeAction.class.php
@@ -1,216 +1,52 @@
 <?php
 class subscribeAction extends commonAction{
-	
 	public function mine(){
-		$userinfo=D("Users")->getUserInfo($this->userid);
-		$registerTime = strtotime($userinfo['addtime']);
-		$is_member = $this->userinfo['if_member'];
-		$dataOffset = time() - $registerTime;
-		$displayNewUserItem = true;
-		if($dataOffset/(3600 *24) > 7){
-			$displayNewUserItem = false;
-		}
-		$this->assign("displayNewUserItem",$displayNewUserItem);
-		$userid = $this->userid;
-		$userType = "0";
-		import("ORG.Util.Page");
-		$usersProductsCategorySubscribe = D("UsersProductsCategorySubscribe");
-		$subscribes = $usersProductsCategorySubscribe->getByUserId($userid);
-		if(count($subscribes) == 0){
-			$this->error("please subscribe prodcut category!");
-		}
-		$categoryIds = "";
-		for($i=0; $i<count($subscribes); $i++){
-			$categoryIds .= $subscribes[$i]["product_category_id"];
-			if($i != count($subscribes)-1){
-				$categoryIds .= ",";
-			}
-		}
-		$sql = "select count(distinct(p.pid)) as count from products  as p, product_effect_relation as per where p.pid=per.pid and FIND_IN_SET(".$userType.",p.user_type) and p.firstcid in (" .$categoryIds .")";
-		$model= new Model();
-		$productCount = $model->query($sql);
-		$count = $productCount[0]['count'];
-		$p = new Page($count,8);
-		$pageSql = $p->firstRow.','.$p->listRows;
-		$productSql = "select distinct(p.pid) as productIds from products  as p, product_effect_relation as per where p.pid=per.pid and FIND_IN_SET(".$userType.",p.user_type) and p.firstcid in (" .$categoryIds .")" ."order by p.pid limit " . $pageSql;
-		$prodcutIdList = $model->query($productSql);
-		$productIdsStr = "";
-		for($i=0; $i<count($prodcutIdList); $i++){
-			$productIdsStr .= $prodcutIdList[$i]["productIds"];
-			if($i != count($prodcutIdList)-1){
-				$productIdsStr .= ",";
-			}	
-		}
-		$where = array("in", $productIdsStr);
-		$products = D("Products")->where("pid in (".$productIdsStr.")")->select();
-		$page=$p->show();	
-		$this->assign('page',$page);
+
+		$this->assign("displayNewUserItem",$this->isNewMember());
+        $products = $this->getAllProductsAndShowPage("0");
+		$products = $this->formatAllProductPrice($products); 
 		$this->assign("list",$products);
-		$this->assign("ismember",$is_member);
+		$this->assign("ismember",$this->userinfo['if_member']);
 		$this->display();
 	}
 	
-	
-	
-	
-	public function year(){
-		$userinfo=D("Users")->getUserInfo($_COOKIE['loli_userid']);
-		$registerTime = strtotime($userinfo['addtime']);
-		$dataOffset = time() - $registerTime;
-		$displayNewUserItem = true;
-		if($dataOffset/(3600 *24) > 7){
-			$displayNewUserItem = false;
-		}
-		$this->assign("displayNewUserItem",$displayNewUserItem);
-		$userid = $this->userid;
-		$userType = "2";
-		import("ORG.Util.Page");
-		$usersProductsCategorySubscribe = D("UsersProductsCategorySubscribe");
-		$subscribes = $usersProductsCategorySubscribe->getByUserId($userid);
-		if(count($subscribes) == 0){
-			$this->error("please subscribe prodcut category!");
-		}
-		$categoryIds = "";
-		for($i=0; $i<count($subscribes); $i++){
-			$categoryIds .= $subscribes[$i]["product_category_id"];
-			if($i != count($subscribes)-1){
-				$categoryIds .= ",";
-			}
-		}
-		$sql = "select count(distinct(p.pid)) as count from products  as p, product_effect_relation as per where p.pid=per.pid and FIND_IN_SET(".$userType.",p.user_type) and p.firtstcid in (" .$categoryIds .")";
-		$model= new Model();
-		$productCount = $model->query($sql);
-		$count = $productCount[0]['count'];
-		$p = new Page($count,8);
-		$pageSql = $p->firstRow.','.$p->listRows;
-		$productSql = "select distinct(p.pid) as productIds from products  as p, product_effect_relation as per where p.pid=per.pid and FIND_IN_SET(".$userType.",p.user_type) and p.firtstcid in (" .$categoryIds .")" ."order by p.pid limit " . $pageSql;
-		$prodcutIdList = $model->query($productSql);
-		$productIdsStr = "";
-		for($i=0; $i<count($prodcutIdList); $i++){
-			$productIdsStr .= $prodcutIdList[$i]["productIds"];
-			if($i != count($prodcutIdList)-1){
-				$productIdsStr .= ",";
-			}	
-		}
-		$where = array("in", $productIdsStr);
-		$products = D("Products")->where("pid in (".$productIdsStr.")")->select();
-		$page=$p->show();	
-		$this->assign('page',$page);
-		$this->assign("list",$products);
-		$this->display();
-	}
+
 	
 	public function newuser(){
-		$userinfo=D("Users")->getUserInfo($this->userid);
-		$registerTime = strtotime($userinfo['addtime']);
-		$is_member = $this->userinfo['if_member'];
-		$endTime = $registerTime + 3600 *24 *7;
-		$dataOffset = $endTime - time();
-		$dayOffSet = floor($dataOffset/(3600 *24));
-		$hourOffSet = floor(($dataOffset - $dayOffSet*24*3600)/3600);
-		$minOffSet = floor(($dataOffset - $dayOffSet*24*3600 - $hourOffSet*3600)/60);
-		$userid = $this->userid;
-		$userType = "3";
-		import("ORG.Util.Page");
-		$usersProductsCategorySubscribe = D("UsersProductsCategorySubscribe");
-		$subscribes = $usersProductsCategorySubscribe->getByUserId($userid);
-		if(count($subscribes) == 0){
-			$this->error("please subscribe prodcut category!");
-		}
-		$categoryIds = "";
-		for($i=0; $i<count($subscribes); $i++){
-			$categoryIds .= $subscribes[$i]["product_category_id"];
-			if($i != count($subscribes)-1){
-				$categoryIds .= ",";
-			}
-		}
-		$sql = "select count(distinct(p.pid)) as count from products  as p, product_effect_relation as per where p.pid=per.pid and FIND_IN_SET(".$userType.",p.user_type) and p.firstcid in (" .$categoryIds .")";
-		$model= new Model();
-		$productCount = $model->query($sql);
-		$count = $productCount[0]['count'];
-		$p = new Page($count,8);
-		$pageSql = $p->firstRow.','.$p->listRows;
-		$productSql = "select distinct(p.pid) as productIds from products  as p, product_effect_relation as per where p.pid=per.pid and FIND_IN_SET(".$userType.",p.user_type) and p.firstcid in (" .$categoryIds .")" ."order by p.pid limit " . $pageSql;
-		$prodcutIdList = $model->query($productSql);
-		$productIdsStr = "";
-		for($i=0; $i<count($prodcutIdList); $i++){
-			$productIdsStr .= $prodcutIdList[$i]["productIds"];
-			if($i != count($prodcutIdList)-1){
-				$productIdsStr .= ",";
-			}	
-		}
-		$where = array("in", $productIdsStr);
-		$products = D("Products")->where("pid in (".$productIdsStr.")")->select();
-		$page=$p->show();	
-		$this->assign("endTime",$endTime);
-		$dayOffSetStr=$dayOffSet;
-		$hourOffSetStr=$hourOffSet;
-		$minOffSetStr=$minOffSet;
-
-		if($hourOffSet<10){
-			$hourOffSetStr = "0".$hourOffSetStr;
-		}
-		if($minOffSetStr<10){
-			$minOffSetStr = "0".$minOffSetStr;
-		}
-		$this->assign("dayOffset",$dayOffSetStr);
-		$this->assign("hourOffset",$hourOffSetStr);
-		$this->assign("minOffset",$minOffSetStr);
-		$this->assign('page',$page);
+		
+		
+		$products = $this->getAllProductsAndShowPage("3");
+		
+		$products = $this->formatAllProductPrice($products);
+	    $this->showCountDown();
 		$this->assign("list",$products);
-		$this->assign("ismember",$is_member);
+		$this->assign("ismember",$this->userinfo['if_member']);
 		
 		$this->display();
 	}	
 	
 	public function advance(){
-		$userinfo=D("Users")->getUserInfo($this->userid);
-		$registerTime = strtotime($userinfo['addtime']);
-		$is_member = $this->userinfo['if_member'];
-		$dataOffset = time() - $registerTime;
-		$displayNewUserItem = true;
-		if($dataOffset/(3600 *24) > 7){
-			$displayNewUserItem = false;
-		}
-		$this->assign("displayNewUserItem",$displayNewUserItem);		$userid = $this->userid;
-		$userType = "1";
-		import("ORG.Util.Page");
-		$usersProductsCategorySubscribe = D("UsersProductsCategorySubscribe");
-		$subscribes = $usersProductsCategorySubscribe->getByUserId($userid);
-		if(count($subscribes) == 0){
-			$this->error("please subscribe prodcut category!");
-		}
-		$categoryIds = "";
-		for($i=0; $i<count($subscribes); $i++){
-			$categoryIds .= $subscribes[$i]["product_category_id"];
-			if($i != count($subscribes)-1){
-				$categoryIds .= ",";
-			}
-		}
-		$sql = "select count(distinct(p.pid)) as count from products  as p, product_effect_relation as per where p.pid=per.pid and FIND_IN_SET(".$userType.",p.user_type) and p.firstcid in (" .$categoryIds .")";
-		$model= new Model();
-		$productCount = $model->query($sql);
-		$count = $productCount[0]['count'];
-		$p = new Page($count,8);
-		$pageSql = $p->firstRow.','.$p->listRows;
-		$productSql = "select distinct(p.pid) as productIds from products  as p, product_effect_relation as per where p.pid=per.pid and FIND_IN_SET(".$userType.",p.user_type) and p.firstcid in (" .$categoryIds .")" ."order by p.pid limit " . $pageSql;
-		$prodcutIdList = $model->query($productSql);
-		$productIdsStr = "";
-		for($i=0; $i<count($prodcutIdList); $i++){
-			$productIdsStr .= $prodcutIdList[$i]["productIds"];
-			if($i != count($prodcutIdList)-1){
-				$productIdsStr .= ",";
-			}	
-		}
-		$where = array("in", $productIdsStr);
-		$products = D("Products")->where("pid in (".$productIdsStr.")")->select();
-		$page=$p->show();	
-		$this->assign('page',$page);
+		$this->assign("displayNewUserItem",$this->isNewMember());		
+		$products = $this->getAllProductsAndShowPage("1");		
+		$products = $this->formatAllProductPrice($products);			
 		$this->assign("list",$products);
-		$this->assign("ismember",$is_member);
+		$this->assign("ismember",$this->userinfo['if_member']);
 		$this->display();
 	}
+	
+	
+	
+	public function year(){
 
+		$this->assign("displayNewUserItem",$this->isNewMember());
+		$userid = $this->userid;
+        $products = $this->getAllProductsAndShowPage("2");        
+		$products = $this->formatAllProductPrice($products);
+		$this->assign("ismember",$this->userinfo['if_member']);
+		$this->assign("list",$products);
+		$this->display();
+	}
+	
     public function subscribeCategory(){
         $categoryIds = $_POST["categoryIds"];
         $userid = $this->userid;
@@ -230,4 +66,94 @@ class subscribeAction extends commonAction{
         }
         $this->ajaxReturn(0,'订阅成功',1);
     }
+    
+    public function isNewMember(){
+    	$userinfo=D("Users")->getUserInfo($this->userid);
+		$registerTime = strtotime($userinfo['addtime']);
+		$dataOffset = time() - $registerTime;
+		$isNewMember = true;
+		if($dataOffset/(3600 *24) > 7){
+			$isNewMember = false;
+		}
+		return $isNewMember;
+    }
+    
+    private function showCountDown(){
+    	$userinfo=D("Users")->getUserInfo($this->userid);
+		$registerTime = strtotime($userinfo['addtime']);
+		$endTime = $registerTime + 3600 *24 *7;
+		$dataOffset = $endTime - time();
+		$dayOffSet = floor($dataOffset/(3600 *24));
+		$hourOffSet = floor(($dataOffset - $dayOffSet*24*3600)/3600);
+		$minOffSet = floor(($dataOffset - $dayOffSet*24*3600 - $hourOffSet*3600)/60);
+				
+		$this->assign("endTime",$endTime);
+		$dayOffSetStr=$dayOffSet;
+		$hourOffSetStr=$hourOffSet;
+		$minOffSetStr=$minOffSet;
+
+		if($hourOffSet<10){
+			$hourOffSetStr = "0".$hourOffSetStr;
+		}
+		if($minOffSetStr<10){
+			$minOffSetStr = "0".$minOffSetStr;
+		}
+		
+		$this->assign("dayOffset",$dayOffSetStr);
+		$this->assign("hourOffset",$hourOffSetStr);
+		$this->assign("minOffset",$minOffSetStr);
+    }
+	private function formatProductPrice($productsArray,$func) {
+	     $productsResult = array(); 
+	     foreach($productsArray as $product) {
+	         if($productResult = $func($product)) {
+	             $productsResult[] = $productResult;
+	         }
+	     }
+	     return $productsResult;
+	}
+	 private function formatAllProductPrice($products) {
+	 	  $func1 = '$product["price"]= $product["price"]/100; $product["member_price"] = $product["member_price"]/100; return $product;';
+		  return $this->formatProductPrice($products,create_function('$product',$func1));
+	
+	}
+	
+	private function getAllProductsAndShowPage($productType){
+		$userid = $this->userid;
+		import("ORG.Util.Page");
+		$usersProductsCategorySubscribe = D("UsersProductsCategorySubscribe");
+		$subscribes = $usersProductsCategorySubscribe->getByUserId($userid);
+		if(count($subscribes) == 0){
+			$this->error("please subscribe prodcut category!");
+		}
+		$categoryIds = "";
+		for($i=0; $i<count($subscribes); $i++){
+			$categoryIds .= $subscribes[$i]["product_category_id"];
+			if($i != count($subscribes)-1){
+				$categoryIds .= ",";
+			}
+		}
+		$sql = "select count(distinct(p.pid)) as count from products  as p, product_effect_relation as per where p.pid=per.pid and FIND_IN_SET(".$productType.",p.user_type) and p.firstcid in (" .$categoryIds .")";
+		$model= new Model();
+		$productCount = $model->query($sql);
+		$count = $productCount[0]['count'];
+		$p = new Page($count,8);
+		$pageSql = $p->firstRow.','.$p->listRows;
+		$productSql = "select distinct(p.pid) as productIds from products  as p, product_effect_relation as per where p.pid=per.pid and FIND_IN_SET(".$productType.",p.user_type) and p.firstcid in (" .$categoryIds .")" ."order by p.pid limit " . $pageSql;
+		$prodcutIdList = $model->query($productSql);
+		$productIdsStr = "";
+		for($i=0; $i<count($prodcutIdList); $i++){
+			$productIdsStr .= $prodcutIdList[$i]["productIds"];
+			if($i != count($prodcutIdList)-1){
+				$productIdsStr .= ",";
+			}	
+		}
+		$where = array("in", $productIdsStr);
+		$products = D("Products")->where("pid in (".$productIdsStr.")")->select();
+		$page=$p->show();
+		$this->assign('page',$page);
+		return $products;
+	}
+	
+	
 }
